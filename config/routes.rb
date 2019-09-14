1d233cf56a1c999517dc52a3a7ef99b431cdc30d
zacharyirlbeck <zachcdmx@gmail.com>
2019-09-14 07:44:55 -0500

    Sets up controller, view, and route for chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'chatrooms/index'
+  get 'chatrooms/show'
+  get 'chatrooms/create'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :chatroom
+
+  root 'chatrooms#index'
 end
