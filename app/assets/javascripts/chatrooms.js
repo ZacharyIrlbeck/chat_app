7539e1d9f71394a486d50669201219020883eaa6
zacharyirlbeck <zachcdmx@gmail.com>
2019-09-14 08:25:18 -0500

    sets up action cable connection

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// All this logic will automatically be available in application.js
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const messageInput = document.querySelector("input[name='message']")
+
+  messageInput.addEventListener("keydown", (event) => {
+  	if(event.keyCode === 13){
+  		event.preventDefault()
+  		console.log("hit enter")
+  	}
+  })
+})
