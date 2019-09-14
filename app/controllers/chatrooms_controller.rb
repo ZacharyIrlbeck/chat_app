19997628448a3b573947d79961ba311a6097343d
zacharyirlbeck <zachcdmx@gmail.com>
2019-09-14 07:54:21 -0500

    Removes multiple chatrooms actionss in favor of one chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,19 +1,5 @@
 class ChatroomsController < ApplicationController
   def index
-  	@chatrooms = Chatroom.all
-  end
-
-  def show
-  	@chatroom = Chatroom.find(params[:id])
-  end
-
-  def create
-  	@chatroom = Chatroom.create permitted_params
-  end
-
-  private
-
-  def permitted_params
-  	params.require(:chatroom).permit(:name)
+  	@chatroom = Chatroom.first
   end
 end
